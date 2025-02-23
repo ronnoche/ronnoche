27603a772d343716e9df9c295fe832e9f39c2b1a
Ron <ronnoche15@gmail.com>
2025-02-23 21:37:30 +0800

    Move posts.json to dedicated posts directory and update fetch path

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function filterPosts() {
 }
 
 // Initialize
-fetch('posts.json')
+fetch('/posts/posts.json')
     .then(response => response.json())
     .then(posts => {
         // Sort posts by date (latest first)
